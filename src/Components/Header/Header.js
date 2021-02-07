615b8dd038f1cc5dfecabf540942c4810786d5bf
KVN2099 <kevinromerooviedo@gmail.com>
2021-02-07 17:49:47 -0600

    Fixed repeated code issues

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -18,36 +18,32 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function Header() {
-    const classes = useStyles();
-    const theme = createMuiTheme({
-        palette: {
-          primary: {
-            main: '#D9E4FB',
-          },
-          secondary: {
-            main: '#f44336',
-          },
+  const classes = useStyles();
+  const theme = createMuiTheme({
+      palette: {
+        primary: {
+          main: '#D9E4FB',
         },
-      });
-    return (
-        <div className="header-container">
-            <Container style={{ display: 'flex' }} maxWidth="md">
-                <Box className="header-icon" component="span" m={1}>
-                    <Button>Logo</Button>
-                </Box>
-                <Box className="header-buttons" component="span" m={1}>
-                    <Button href="#about">About</Button>
-                    <Button href="#projects">Projects</Button>
-                    <Button href="#coding-challenges">Coding Challenges</Button>
-                    <Button href="#contact" variant="outlined" color="primary">Contact</Button>
-                    <Button>About</Button>
-                    <Button>Projects</Button>
-                    <Button>Coding Challenges</Button>
-                    <Button size="small" variant="outlined" color="primary">Contact</Button>
-                </Box>
-            </Container>
-        </div>
+        secondary: {
+          main: '#f44336',
+        },
+      },
+  });
+  return (
+      <div className="header-container">
+          <Container style={{ display: 'flex' }} maxWidth="md">
+              <Box className="header-icon" component="span" m={1}>
+                  <Button href="#about">Logo</Button>
+              </Box>
+              <Box className="header-buttons" component="span" m={1}>
+                  <Button href="#about">About</Button>
+                  <Button href="#projects">Projects</Button>
+                  <Button href="#coding-challenges">Coding Challenges</Button>
+                  <Button href="#contact" variant="outlined" color="primary">Contact</Button>
+              </Box>
+          </Container>
+      </div>
     )
-}
+};
 
-export default Header
+export default Header;
