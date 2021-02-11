894eea10ac3a4b6e0a8bea629b90cf40b19356ce
KVN2099 <kevinromerooviedo@gmail.com>
2021-02-11 00:04:30 -0600

    Contact form is complete. Still missing social media icons.

--- src/Slides/Contact Me/Contact.js
+++ src/Slides/Contact Me/Contact.js
@@ -1,11 +1,28 @@
-import { Box, Button, makeStyles, Paper, TextField, ThemeProvider } from '@material-ui/core';
-import React from 'react';
+import { Box, Button, makeStyles, Paper, Snackbar, TextField } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
+import React, { useState } from 'react';
 import ContainerLayout from '../../Components/Container/Container';
 import emailjs from 'emailjs-com';
 import colors from '../../Themes/Light Theme/styles';
+import SendIcon from '@material-ui/icons/Send';
 import './Contact.css';
 
 function Contact() {
+    const [open, setOpen] = useState(false);
+    const [ alertMessage, setAlertMessage ] = useState("");
+    const [ severity, setSeverity ] = useState("");
+
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+        return;
+        }
+
+        setOpen(false);
+    };
     const useStyles = makeStyles((theme) => ({
         root: {
           display: 'flex',
@@ -16,22 +33,56 @@ function Contact() {
             height: theme.spacing(16),
           }
         },
+        button: {
+            margin: theme.spacing(1),
+          },
       }));
     
     const classes = useStyles();
 
+    function setError(error) {
+        setSeverity("error");
+        handleClick();
+    }
+
     function sendEmail(e) {
         e.preventDefault();
-        emailjs.sendForm('service_6ydxn08', 'template_bqlcpy2', e.target, 'user_PXTlX7vDwNbtkwjS3hGg1')
-            .then((result) => {
-                alert(result.text);
-                console.log(result.text);
-            }, (error) => {
-                alert(error.text);
-                console.log(error.text);
-            });
-            e.target.reset();
+        if (validateFields(e.target)) {
+            emailjs.sendForm('service_6ydxn08', 'template_bqlcpy2', e.target, 'user_PXTlX7vDwNbtkwjS3hGg1')
+                .then((result) => {
+                    setSeverity("success");
+                    setAlertMessage('Email sent! I will get back to you soon.');
+                    handleClick();
+                    console.log(result.text);
+                }, (error) => {
+                    setAlertMessage("An error ocurred. Please try again.")
+                    setError();
+                });
+                e.target.reset();
+        } else {
+            setError();
         }
+    }
+
+    function Alert(props) {
+        return <MuiAlert elevation={6} variant="filled" {...props} />;
+    }
+    function validateFields(fields) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(String(fields.from_name.value).toLowerCase())) {
+            setAlertMessage("Please use a valid email address.")
+            return false;
+        }
+        if (fields._subject.value.replace(/\s+/g, '') == "") {
+            setAlertMessage("Please type a subject.")
+            return false;
+        }
+        if (fields.message.value.replace(/\s+/g, '') == "") {
+            setAlertMessage("Please send a message.")
+            return false;
+        }
+        return true;
+    }
     return (
         <ContainerLayout className="container-slide" id="contact" background={ colors.lightBackground }>
             <Box className="container-content">
@@ -41,7 +92,20 @@ function Contact() {
                         <TextField className="material-input" name="from_name" size="small" id="outlined-basic" label="Your email address" variant="outlined" />
                         <TextField className="material-input" name="_subject" size="small" id="outlined-basic" label="Subject" variant="outlined" />
                         <TextField className="material-input" name="message" size="small" multiline rows="4" id="outlined-basic" label="Message body" variant="outlined" />
-                        <button type="submit">Send</button>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="secondary"
+                            className={ classes.button }
+                            startIcon={ <SendIcon /> }
+                        >
+                            Send Email
+                        </Button>
+                        <Snackbar open={ open } autoHideDuration={ 6000 } onClose={ handleClose }>
+                            <Alert onClose={ handleClose } severity={ severity }>
+                                { alertMessage }
+                            </Alert>
+                        </Snackbar>
                     </form>
                 </Paper>
             </Box>
