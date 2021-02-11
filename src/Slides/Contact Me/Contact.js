34107ad9b3f00a11606fe3e0c7727c55f07b09b0
KVN2099 <kevinromerooviedo@gmail.com>
2021-02-10 22:07:52 -0600

    Created contact form with emailJS

--- src/Slides/Contact Me/Contact.js
+++ src/Slides/Contact Me/Contact.js
@@ -1,8 +1,7 @@
-import { Box, Button, makeStyles, Paper, ThemeProvider } from '@material-ui/core';
+import { Box, Button, makeStyles, Paper, TextField, ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import ContainerLayout from '../../Components/Container/Container';
-import ContentBox from '../../Components/ContentBox/ContentBox';
-import theme from '../../Themes/Light Theme/LightTheme';
+import emailjs from 'emailjs-com';
 import colors from '../../Themes/Light Theme/styles';
 import './Contact.css';
 
@@ -20,25 +19,31 @@ function Contact() {
       }));
     
     const classes = useStyles();
+
+    function sendEmail(e) {
+        e.preventDefault();
+        emailjs.sendForm('service_6ydxn08', 'template_bqlcpy2', e.target, 'user_PXTlX7vDwNbtkwjS3hGg1')
+            .then((result) => {
+                alert(result.text);
+                console.log(result.text);
+            }, (error) => {
+                alert(error.text);
+                console.log(error.text);
+            });
+            e.target.reset();
+        }
     return (
         <ContainerLayout className="container-slide" id="contact" background={ colors.lightBackground }>
             <Box className="container-content">
-                <ContentBox className={ classes.root } size="small">
-                    <Paper className="paper" elevation={3}>
-                        <div>
+                <Paper className="paper-form" elevation={3}>
+                    <form onSubmit={ sendEmail }>
                         <h1>Get in Touch</h1>
-                        <ThemeProvider theme={ theme }>
-                            <Button href="#projects" variant="contained" color="primary" disableElevation>
-                            Check Out My Projects!
-                            </Button>
-                            <Button href="#contact" variant="contained" color="secondary" disableElevation>
-                            And my social media
-                            </Button>
-                        </ThemeProvider>
-                        </div>
-                    </Paper>
-                </ContentBox>
-                <ContentBox size="large" style={{ backgroundColor: 'green' }}></ContentBox>
+                        <TextField className="material-input" name="from_name" size="small" id="outlined-basic" label="Your email address" variant="outlined" />
+                        <TextField className="material-input" name="_subject" size="small" id="outlined-basic" label="Subject" variant="outlined" />
+                        <TextField className="material-input" name="message" size="small" multiline rows="4" id="outlined-basic" label="Message body" variant="outlined" />
+                        <button type="submit">Send</button>
+                    </form>
+                </Paper>
             </Box>
         </ContainerLayout>
     )
